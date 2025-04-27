3a150b000a95e6da61961b99d14d1dc2b2731245
STCGMS1 <mhondiwaesroge@gmail.com>
2025-04-27 14:39:57 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Burial Records Management</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Burial Records Form</h1>
